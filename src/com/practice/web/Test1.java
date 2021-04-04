1bcf0c4eef884f226ba8f07ef7b59b7cc246bf38
J1NWAN <weamicron@gmail.com>
2021-04-04 23:19:58 +0900

    자바 서블릿 연습용 !

---
+++ src/com/practice/web/Test1.java
@@ -0,0 +1,20 @@
+package com.practice.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test1 extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("Hello Servlet");
+	}
+}
